19a6dd99a8c70d3e7b5435abd719e50ab3e910cd
aspol12-real <furniturepolish80@gmail.com>
2025-10-29 22:01:49 -0230

    added another assembler rom example & added alias + const support (fixed some bugs aswell)

--- dev/assembler.py
+++ dev/assembler.py
@@ -2,7 +2,9 @@
 file_name = input("filename: ")
 address = 512
 label_table = {}
+const_table = {}
 generated_bytes = []
+
 
 #pass 1, figure out labels and macros
 
@@ -23,11 +25,18 @@ for line in lines:
     if not tokens:
         continue
 
+    if tokens[0] == ".const" or tokens[0] == ".alias":
+        if len(tokens) != 3:
+            print("CONST syntax error!")
+        const_name = tokens[1]
+        const_value = tokens[2]
+        const_table[const_name] = const_value
+        continue
+
     if ":" in tokens[0]:
         label_name = tokens[0].rstrip(":")
         label_table[label_name] = address
-        if tokens[-1].endswith(':'): 
-            continue
+        continue
 
     elif tokens[0] == ".byte":
         num_bytes = len(tokens) - 1 
@@ -53,6 +62,14 @@ for line in lines:
     if not tokens:
         continue
 
+    new_tokens = []
+    for token in tokens:
+        if token in const_table:
+            new_tokens.append(const_table[token])
+        else:
+            new_tokens.append(token)
+    tokens = new_tokens
+
     if tokens[0] == ".byte":
         data_values = tokens[1:] 
 
@@ -62,54 +79,54 @@ for line in lines:
 
         continue
 
-    elif tokens[0] == "scd":
-        n = int(tokens[1], 0)
+    elif new_tokens[0] == "scd":
+        n = int(new_tokens[1], 0)
         b = [0x00, 0xC0 | n]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "scu":
-        n = int(tokens[1], 0)
+    elif new_tokens[0] == "scu":
+        n = int(new_tokens[1], 0)
         b = [0x00, 0xD0 | n]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "cls":
+    elif new_tokens[0] == "cls":
         b = [0x00, 0xE0]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "ret":
+    elif new_tokens[0] == "ret":
         b = [0x00, 0xEE]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "scr":
+    elif new_tokens[0] == "scr":
         b = [0x00, 0xFB]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "scl":
+    elif new_tokens[0] == "scl":
         b = [0x00, 0xFC]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "lores":
+    elif new_tokens[0] == "lores":
         b = [0x00, 0xFE]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "hires":
+    elif new_tokens[0] == "hires":
         b = [0x00, 0xFF]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "jmp":
-        target = tokens[1]
+    elif new_tokens[0] == "jmp":
+        target = new_tokens[1]
 
         if target in label_table:
             nnn = label_table[target]
         else:
             print(f"Target label: {target} not found!")
-            continue
+            exit()
 
         b = [0x10 | (nnn >> 8) & 0x0f, nnn & 0xFF]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "call":
-        target = tokens[1]
+    elif new_tokens[0] == "call":
+        target = new_tokens[1]
 
         if target in label_table:
             nnn = label_table[target]
@@ -120,204 +137,205 @@ for line in lines:
         b = [0x20 | (nnn >> 8) & 0x0F, nnn & 0xFF]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "se":
-        x = int(tokens[1], 0)
-        nn = int(tokens[2], 0)
+    elif new_tokens[0] == "se":
+        x = int(new_tokens[1], 0)
+        nn = int(new_tokens[2], 0)
         b = [0x30 | (x & 0x0F), nn]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "sne":
-        x = int(tokens[1], 0)
-        nn = int(tokens[2], 0)
+    elif new_tokens[0] == "sne":
+        x = int(new_tokens[1], 0)
+        nn = int(new_tokens[2], 0)
         b = [0x40 | (x & 0x0F), nn]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "ser":
-        x = int(tokens[1], 0)
-        nn = int(tokens[2], 0)
+    elif new_tokens[0] == "ser":
+        x = int(new_tokens[1], 0)
+        nn = int(new_tokens[2], 0)
         b = [0x50 | (x & 0x0F), (y << 4) | 0x00]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "ld":
-        x = int(tokens[1], 0)
-        nn = int(tokens[2], 0)
+    elif new_tokens[0] == "ld":
+        x = int(new_tokens[1], 0)
+        nn = int(new_tokens[2], 0)
         b = [0x60 | (x & 0x0F), nn]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "addi":
-        x = int(tokens[1], 0)
-        nn = int(tokens[2], 0)
+    elif new_tokens[0] == "addi":
+        x = int(new_tokens[1], 0)
+        nn = int(new_tokens[2], 0)
         b = [0x70 | (x & 0x0F), nn]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "ldr":
-        x = int(tokens[1], 0)
-        y = int(tokens[2], 0)
+    elif new_tokens[0] == "ldr":
+        x = int(new_tokens[1], 0)
+        y = int(new_tokens[2], 0)
         b = [0x80 | (x & 0x0F), (y << 4) | 0x00]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "or":
-        x = int(tokens[1], 0)
-        y = int(tokens[2], 0)
+    elif new_tokens[0] == "or":
+        x = int(new_tokens[1], 0)
+        y = int(new_tokens[2], 0)
         b = [0x80 | (x & 0x0F), (y << 4) | 0x01]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "and":
-        x = int(tokens[1], 0)
-        y = int(tokens[2], 0)
+    elif new_tokens[0] == "and":
+        x = int(new_tokens[1], 0)
+        y = int(new_tokens[2], 0)
         b = [0x80 | (x & 0x0F), (y << 4) | 0x02]
         generated_bytes.extend(b)        
 
-    elif tokens[0] == "xor":
-        x = int(tokens[1], 0)
-        y = int(tokens[2], 0)
+    elif new_tokens[0] == "xor":
+        x = int(new_tokens[1], 0)
+        y = int(new_tokens[2], 0)
         b = [0x80 | (x & 0x0F), (y << 4) | 0x03]
         generated_bytes.extend(b)        
 
-    elif tokens[0] == "add":
-        x = int(tokens[1], 0)
-        y = int(tokens[2], 0)
+    elif new_tokens[0] == "add":
+        x = int(new_tokens[1], 0)
+        y = int(new_tokens[2], 0)
         b = [0x80 | (x & 0x0F), (y << 4) | 0x04]
         generated_bytes.extend(b)        
 
-    elif tokens[0] == "sub":
-        x = int(tokens[1], 0)
-        y = int(tokens[2], 0)
+    elif new_tokens[0] == "sub":
+        x = int(new_tokens[1], 0)
+        y = int(new_tokens[2], 0)
         b = [0x80 | (x & 0x0F), (y << 4) | 0x05]
         generated_bytes.extend(b)        
 
-    elif tokens[0] == "shr":
-        x = int(tokens[1], 0)
-        y = int(tokens[2], 0)
+    elif new_tokens[0] == "shr":
+        x = int(new_tokens[1], 0)
+        y = int(new_tokens[2], 0)
         b = [0x80 | (x & 0x0F), (y << 4) | 0x06]
         generated_bytes.extend(b)        
 
-    elif tokens[0] == "subn":
-        x = int(tokens[1], 0)
-        y = int(tokens[2], 0)
+    elif new_tokens[0] == "subn":
+        x = int(new_tokens[1], 0)
+        y = int(new_tokens[2], 0)
         b = [0x80 | (x & 0x0F), (y << 4) | 0x07]
         generated_bytes.extend(b)        
 
-    elif tokens[0] == "shl":
-        x = int(tokens[1], 0)
-        y = int(tokens[2], 0)
+    elif new_tokens[0] == "shl":
+        x = int(new_tokens[1], 0)
+        y = int(new_tokens[2], 0)
         b = [0x80 | (x & 0x0F), (y << 4) | 0x0E]
         generated_bytes.extend(b)        
 
-    elif tokens[0] == "sner":
-        x = int(tokens[1], 0)
-        y = int(tokens[2], 0)
+    elif new_tokens[0] == "sner":
+        x = int(new_tokens[1], 0)
+        y = int(new_tokens[2], 0)
         b = [0x90 | (x & 0x0F), (y << 4) | 0x00]
         generated_bytes.extend(b)  
 
-    elif tokens[0] == "ldi":
+    elif new_tokens[0] == "ldi":
 
-        target = tokens[1]
+        target = new_tokens[1]
 
         if target in label_table:
             nnn = label_table[target]
         else:
-            nnn = int(tokens[1], 0)
+            nnn = int(new_tokens[1], 0)
 
-        b = [0xA0 | (nnn >> 8), (nnn & 0xFF)]
+        b = [0xA0 | ((nnn >> 8) & 0x0F), (nnn & 0xFF)]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "jmpv":
-        nnn = int(tokens[1], 0)
+    elif new_tokens[0] == "jmpv":
+        nnn = int(new_tokens[1], 0)
         b = [0xB0 | (nnn >> 8), (nnn & 0xFF)]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "random":
-        x = int(tokens[1], 0)
-        nn = int(tokens[2], 0)
-        b = [0xC0 | (x & 0xF) | nn]
+    elif new_tokens[0] == "random":
+        x = int(new_tokens[1], 0)
+        nn = int(new_tokens[2], 0)
+        b = [0xC0 | (x & 0xF),  nn]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "draw":
-        x = int(tokens[1], 0)
-        y = int(tokens[2], 0)
-        n = int(tokens[3], 0)
+    elif new_tokens[0] == "draw":
+        x = int(new_tokens[1], 0)
+        y = int(new_tokens[2], 0)
+        n = int(new_tokens[3], 0)
         b = [0xD0 | x, (y << 4) | n]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "bigdraw":
-        x = int(tokens[1], 0)
-        y = int(tokens[2], 0)
+    elif new_tokens[0] == "bigdraw":
+        x = int(new_tokens[1], 0)
+        y = int(new_tokens[2], 0)
         b = [0xD0 | x, (y << 4) | 0x00]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "skp":
-        x = int(tokens[1], 0)
+    elif new_tokens[0] == "skp":
+        x = int(new_tokens[1], 0)
         b = [0xE0 | x, 0x9E]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "sknp":
-        x = int(tokens[1], 0)
+    elif new_tokens[0] == "sknp":
+        x = int(new_tokens[1], 0)
         b = [0xE0 | x, 0xA1]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "long":
-        nnnn = tokens[1]
+    elif new_tokens[0] == "long":
+        nnnn = new_tokens[1]
         b = [0xF0, 0x00, (nnnn >> 8) & 0xFF, nnnn & 0xFF]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "plane":
-        x = int(tokens[1], 0)
+    elif new_tokens[0] == "plane":
+        x = int(new_tokens[1], 0)
         b = [0xF0 | x, 0x01]
         generated_bytes.extend(b)
 
-    elif tokens[0] == "getdelay":
-        x = int(tokens[1], 0)
+    elif new_tokens[0] == "getdelay":
+        x = int(new_tokens[1], 0)
         b = [0xF0 | x, 0x07]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "wait":
-        x = int(tokens[1], 0)
+    elif new_tokens[0] == "wait":
+        x = int(new_tokens[1], 0)
         b = [0xF0 | x, 0x0A]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "delay":
-        x = int(tokens[1], 0)
+    elif new_tokens[0] == "delay":
+        x = int(new_tokens[1], 0)
         b = [0xF0 | x, 0x15]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "sound":
-        x = int(tokens[1], 0)
+    elif new_tokens[0] == "sound":
+        x = int(new_tokens[1], 0)
         b = [0xF0 | x, 0x18]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "vxi":
-        x = int(tokens[1], 0)
+    elif new_tokens[0] == "vxi":
+        x = int(new_tokens[1], 0)
         b = [0xF0 | x, 0x1E]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "smallhex":
-        x = int(tokens[1], 0)
+    elif new_tokens[0] == "smallhex":
+        x = int(new_tokens[1], 0)
         b = [0xF0 | x, 0x29]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "bighex":
-        x = int(tokens[1], 0)
+    elif new_tokens[0] == "bighex":
+        x = int(new_tokens[1], 0)
         b = [0xF0 | x, 0x30]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "bcd":
-        x = int(tokens[1], 0)
+    elif new_tokens[0] == "bcd":
+        x = int(new_tokens[1], 0)
         b = [0xF0 | x, 0x33]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "store":
-        x = int(tokens[1], 0)
+    elif new_tokens[0] == "store":
+        x = int(new_tokens[1], 0)
         b = [0xF0 | x, 0x55]
         generated_bytes.extend(b) 
 
-    elif tokens[0] == "read":
-        x = int(tokens[1], 0)
+    elif new_tokens[0] == "read":
+        x = int(new_tokens[1], 0)
         b = [0xF0 | x, 0x65]
         generated_bytes.extend(b) 
 
 
 print(label_table)
+print(const_table)
 
 for byte_value in generated_bytes:
     print(f"{byte_value:02X}", end=" ")
